0b1be00332f7f67d1daa4a14a4121ab4cf1649ad
adel <netdur@gmail.com>
2025-11-27 01:08:56 +0100

    v0.1.3

--- hugind.rb
+++ hugind.rb
@@ -2,7 +2,7 @@ class Hugind < Formula
   desc "Dart-based CLI for LLMs"
   homepage "https://github.com/netdur/hugind"
   # REPLACE THE URL BELOW with your actual GitHub Release asset link
-  url "https://github.com/netdur/hugind/releases/download/v0.1.2/hugind-macos-arm64.tar.gz"
+  url "https://github.com/netdur/hugind/releases/download/v0.1.3/hugind-macos-arm64.tar.gz"
   # REPLACE THE SHA BELOW with the one generated in Step 1
   sha256 "6a8649d0048b2def0741b0c5dab790145d44dbdfab5a23435e36ba898e540423"
   version "0.1.3"
