3cf8d95564329db8dd9e63745f8d63abaf71ef61
adel <netdur@gmail.com>
2026-01-12 16:25:34 +0100

    Update hugind to v0.5.0

--- hugind.rb
+++ hugind.rb
@@ -2,10 +2,10 @@ class Hugind < Formula
   desc "Dart-based CLI for LLMs"
   homepage "https://github.com/netdur/hugind"
   # REPLACE THE URL BELOW with your actual GitHub Release asset link
-  url "https://github.com/netdur/hugind/releases/download/v0.4.0/hugind-macos-arm64.tar.gz"
+  url "https://github.com/netdur/hugind/releases/download/v0.5.0/hugind-macos-arm64.tar.gz"
   # REPLACE THE SHA BELOW with the one generated in Step 1
-  sha256 "3a7591fe39ec85a67d33f8a50bc45a4d001f339d5501145dc4c0e6b8263c8ec3"
-  version "0.4.0"
+  sha256 "a3c47ac8700f91f17d2262fc4acf50c2c8062866249f5ffc534900dc099df847"
+  version "0.5.0"
 
   def install
     # libexec is a private directory where we put all files (binary + libs + config)
